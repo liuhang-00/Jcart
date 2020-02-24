ff86170d4595af85277227006ac27eb76e6e1090
liuhang-00 <1910602774@qq.com>
2020-02-24 18:37:03 +0800

    init jcart-administration-back

---
+++ jcart-administration-back/src/main/java/com/liuhang/jcartadministrationback/JcartAdministrationBackApplication.java
@@ -0,0 +1,13 @@
+package com.liuhang.jcartadministrationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartAdministrationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartAdministrationBackApplication.class, args);
+    }
+
+}
---
+++ jcart-administration-back/src/test/java/com/liuhang/jcartadministrationback/JcartAdministrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuhang.jcartadministrationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartAdministrationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
