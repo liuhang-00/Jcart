da4b100e806923ade71a1a0912c442c93f4bb6e2
liuhang-00 <1910602774@qq.com>
2020-02-24 18:40:39 +0800

    first init jcart-store-back

---
+++ jcart-store-back/src/main/java/com/liuhang/jcartstoreback/JcartStoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.liuhang.jcartstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartStoreBackApplication.class, args);
+    }
+
+}
---
+++ jcart-store-back/src/test/java/com/liuhang/jcartstoreback/JcartStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuhang.jcartstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
