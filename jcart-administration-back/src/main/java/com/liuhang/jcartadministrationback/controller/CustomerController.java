5e863e46220e1623305db33519e22de04f0639f6
liuhang-00 <1910602774@qq.com>
2020-02-25 15:35:25 +0800

    add administrator customerController

---
+++ jcart-administration-back/src/main/java/com/liuhang/jcartadministrationback/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.liuhang.jcartadministrationback.controller;
+
+import com.liuhang.jcartadministrationback.dto.in.CustomerSearchInDTO;
+import com.liuhang.jcartadministrationback.dto.out.CustomerListOutDTO;
+import com.liuhang.jcartadministrationback.dto.out.CustomerShowOutDTO;
+import com.liuhang.jcartadministrationback.dto.out.PageOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    @GetMapping("/getList")
+    public PageOutDTO<CustomerListOutDTO> getList(@RequestBody CustomerSearchInDTO customerSearchInDTO){
+        return null;
+    }
+
+    @GetMapping("/show")
+    public CustomerShowOutDTO show(@RequestParam Integer customerId){
+        return null;
+    }
+
+    @PostMapping("/disable")
+    public void disable(@RequestParam Integer customerId){
+
+    }
+
+    
+}
