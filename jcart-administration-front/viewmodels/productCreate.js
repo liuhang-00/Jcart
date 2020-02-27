9e30a7ed932a6a7dc48d1da065a90ab714ef5a8e
liuhang-00 <1910602774@qq.com>
2020-02-27 21:31:31 +0800

    update administrator front product create

--- jcart-administration-front/viewmodels/productCreate.js
+++ jcart-administration-front/viewmodels/productCreate.js
@@ -1,16 +1,86 @@
 var app = new Vue({
     el: '#app',
     data: {
+        selectedMainPic: '',
+        mainPicUrl: '',
+        selectedOtherPics: [],
+        otherPicUrls: [],
+
         productVo:{},
         statusList:[
             { value: 0, label: '下架' },
             { value: 1, label: '上架' },
             { value: 2, label: '待审核' }
-        ]
+        ],
+        mainFileList: [],
+        otherFileList: []
+
     },methods: {
         onsubmit(){
 
-        }
+        },
+        handleOnMainChange(val) {
+            this.selectedMainPic = val.raw;
+        },
+        handleUploadMainClick() {
+            console.log('upload main pic click');
+            this.uploadMainImage();
+        },
+        uploadMainImage() {
+            var formData = new FormData();
+            formData.append("image", this.selectedMainPic);
+
+            axios.post('/image/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.mainPicUrl = response.data;
+                    alert('上传成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    alert('上传失败');
+                });
+        },
+        handleOnOtherChange(file, fileList) {
+            console.log('fileList', fileList);
+            this.selectedOtherPics = fileList;
+        },
+        handleOnOtherRemove(file, fileList) {
+            console.log('fileList', fileList);
+            this.selectedOtherPics = fileList;
+        },
+        handleUploadOtherClick() {
+            console.log('upload other pic click');
+            this.uploadOtherImage();
+        },
+        uploadOtherImage() {
+            this.selectedOtherPics.forEach(pic => {
+                var formData = new FormData();
+                formData.append("image", pic.raw);
+
+                axios.post('/image/upload', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                    .then(function (response) {
+                        console.log(response);
+                        var url = response.data;
+                        app.otherPicUrls.push(url);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                        alert('上床失败');
+                    });
+            });
+
+
+        },
+
     },mounted() {
         
     },
