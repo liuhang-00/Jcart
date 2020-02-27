85e3cab11650683d8aea7b7a50fc0d20e2bef269
liuhang-00 <1910602774@qq.com>
2020-02-27 19:50:10 +0800

    add administrator front product create

--- jcart-administration-front/viewmodels/productCreate.js
+++ jcart-administration-front/viewmodels/productCreate.js
@@ -1,4 +1,17 @@
 var app = new Vue({
     el: '#app',
-    data: {}
+    data: {
+        productVo:{},
+        statusList:[
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ]
+    },methods: {
+        onsubmit(){
+
+        }
+    },mounted() {
+        
+    },
 })
--- jcart-administration-front/viewmodels/productSearch.js
+++ jcart-administration-front/viewmodels/productSearch.js
@@ -5,22 +5,23 @@ var app = new Vue({
         pageSize:2,
         total:1,
         product:[],
-        productName:'',
-        productCode:'',
-        price:0.00,
-        status:0,
-        stockQuantity:0
+        productVo:{},
+        statusList:[
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ]
     },
     methods: {
         search(){
-            console.log(this.status);
+            console.log(this.productVo.status);
             var params={
                 pageNum:this.pageNum,
-                productName:this.productName,
-                productCode:this.productCode,
-                price:this.price,
-                status:this.status,
-                stockQuantity:this.stockQuantity          
+                productName:this.productVo.productName,
+                productCode:this.productVo.productCode,
+                price:this.productVo.price,
+                status:this.productVo.status,
+                stockQuantity:this.productVo.stockQuantity          
             };
             axios.get("/product/search",{params:params}).then(res=>{
                 this.pageNum=res.data.pageNum;
