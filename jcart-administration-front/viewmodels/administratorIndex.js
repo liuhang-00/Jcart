588ad8df45211619f8b7582ab232419692b60bef
liuhang-00 <1910602774@qq.com>
2020-03-04 22:18:46 +0800

    create administrator front administratordelete

--- jcart-administration-front/viewmodels/administratorIndex.js
+++ jcart-administration-front/viewmodels/administratorIndex.js
@@ -4,7 +4,9 @@ var app = new Vue({
         administrator:[],
         total:0,
         pageSize:2,
-        pageNum:1
+        pageNum:1,
+        selectedAdministrators: []
+
     },methods: {
         currentchange(pageNum){
             this.pageNum=pageNum;
@@ -23,15 +25,48 @@ var app = new Vue({
                 this.pageNum=res.data.pageNum;
                 this.pageSize=res.data.pageSize;
             })
-        },
-        handleDelete(index,row){
+        } ,handleDelete(index, row) {
+            console.log('delete click');
 
+            if (confirm("确认删除？")) {
+                this.deleteAdministrator(row.administratorId);
+            }
         },
-        handleSelectionChange(){
-
+        handleBatchDeleteClick() {
+            console.log('batch delete click');
+            if (confirm("确认删除？")) {
+                this.batchDeleteAdministrators();
+            }
         },
-        handleBatchDeleteClick(){
-
+        handleSelectionChange(val) {
+            console.log('selection change', val);
+            this.selectedAdministrators = val;
+        },
+        deleteAdministrator(administratorId) {
+            axios.post('/administrator/delete', administratorId, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('删除成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        batchDeleteAdministrators() {
+            axios.post('/administrator/batchDelete', this.selectedAdministratorIds)
+                .then(function (response) {
+                    console.log(response);
+                    alert('批删成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
     },mounted() {
         this.getList();
