6c2b7294046247e8ad74a6930489fb1154aba704
liuhang-00 <1910602774@qq.com>
2020-03-15 12:16:11 +0800

    fix administrator back orderController

--- jcart-administration-back/src/main/java/com/liuhang/jcartadministrationback/controller/OrderHistoryController.java
+++ jcart-administration-back/src/main/java/com/liuhang/jcartadministrationback/controller/OrderHistoryController.java
@@ -2,23 +2,47 @@ package com.liuhang.jcartadministrationback.controller;
 
 import com.liuhang.jcartadministrationback.dto.in.OrderHistoryCreateInDTO;
 import com.liuhang.jcartadministrationback.dto.out.OrderHistoryListOutDTO;
+import com.liuhang.jcartadministrationback.po.OrderHistory;
+import com.liuhang.jcartadministrationback.service.OrderHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orderhistory")
 @CrossOrigin
 public class OrderHistoryController {
 
+    @Autowired
+    private OrderHistoryService service;
     @GetMapping("/getListByOrderId")
     public List<OrderHistoryListOutDTO> getListByOrderId(@RequestParam Long orderId){
-        return null;
+        List<OrderHistory> byOrderId = service.getByOrderId(orderId);
+        List<OrderHistoryListOutDTO> orderHistoryListOutDTOS= byOrderId.stream().map(orderHistory -> {
+            OrderHistoryListOutDTO orderHistoryListOutDTO = new OrderHistoryListOutDTO();
+            orderHistoryListOutDTO.setOrderHistoryId(orderHistory.getOrderHistoryId());
+            orderHistoryListOutDTO.setTimestamp(orderHistory.getTime().getTime());
+            orderHistoryListOutDTO.setOrderStatus(orderHistory.getOrderStatus());
+            orderHistoryListOutDTO.setComment(orderHistory.getComment());
+            orderHistoryListOutDTO.setCustomerNotified(orderHistory.getCustomerNotified());
+            return orderHistoryListOutDTO;
+        }).collect(Collectors.toList());
+        return orderHistoryListOutDTOS;
     }
 
     @PostMapping("/create")
-    public Integer create(@RequestBody OrderHistoryCreateInDTO orderHistoryCreateInDTO){
-        return null;
+    public Long create(@RequestBody OrderHistoryCreateInDTO orderHistoryCreateInDTO){
+        OrderHistory orderHistory = new OrderHistory();
+        orderHistory.setOrderId(orderHistoryCreateInDTO.getOrderId());
+        orderHistory.setTime(new Date());
+        orderHistory.setOrderStatus(orderHistoryCreateInDTO.getOrderStatus());
+        orderHistory.setComment(orderHistoryCreateInDTO.getComment());
+        orderHistory.setCustomerNotified(orderHistoryCreateInDTO.getCustomerNotified());
+        Long orderHistoryId = service.craete(orderHistory);
+        return orderHistoryId;
     }
 
 }
